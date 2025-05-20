88f2ffdc03696ad77226fcd7a211153fc959a6af
Harry <68599167+harrykeeran12@users.noreply.github.com>
2025-05-21 00:01:28 +0100

    fix: replaced argparse library with typer.

--- main.py
+++ main.py
@@ -1,37 +1,44 @@
 import random
 import argparse
+from typing_extensions import Annotated
+import typer
+from rich import print
+
+app = typer.Typer()
+
+# parser = argparse.ArgumentParser(
+#     prog="daff",
+#     description="This program prints out daily affirmations.",
+# )
 
 
-parser = argparse.ArgumentParser(
-    prog="daff",
-    description="This program prints out daily affirmations.",
-)
-
-
-def main():
+@app.command()
+def main(number: Annotated[int, typer.Argument()] = 1):
     """Print out a random affirmation per day."""
-    parser.add_argument(
-        "-n",
-        "--number",
-        default=1,
-        type=int,
-        action="store",
-        help="generate a specific number of affirmations",
-    )
+    # parser.add_argument(
+    #     "-n",
+    #     "--number",
+    #     default=1,
+    #     type=int,
+    #     action="store",
+    #     help="generate a specific number of affirmations",
+    # )
 
     with open("affirmations.txt") as f:
         lines = f.readlines()
         random.shuffle(lines)
-    args = parser.parse_args()
+    # args = parser.parse_args()
     # print(args.number)
-    if args.number == 1:
-        print(random.choice(lines).strip())
-    elif args.number > 1:
-        for index in range(1, args.number + 1):
-            print(f"{index}. {random.choice(lines)} ")
+    # number = args.number
+    if number == 1:
+        print(f"[bold]{random.choice(lines).strip()}[/bold]")
+    elif number > 1:
+        for index in range(1, number + 1):
+            print(f"[bold]{index}[/bold]. {random.choice(lines).strip()} ")
     else:
         raise Exception("The number passed in was smaller than 1.")
 
 
 if __name__ == "__main__":
-    main()
+    # typer.run(main)
+    app()
